41a79925504de3a183a9347ca7d25262749e9cd3
Mayuresh Sarode <mayureshsarode61@gmail.com>
2023-02-26 10:45:25 +0530

    Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+	//Store candidate
+	//Read Candidate
+	string public candidate;
+	//Constructor
+	constructor() public {
+		candidate = "Candidate 1";
+	}
+
+}
